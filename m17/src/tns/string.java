2eaf73552a6880bd1227a576b3558da3b7151609
TanishPaul <tanishpaul26@gmail.com>
2022-06-02 10:40:10 +0530

    First Commit

---
+++ m17/src/tns/string.java
@@ -0,0 +1,13 @@
+package tns;
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=new String("Tanish");
+		s.concat("Paul");
+		System.out.println(s);
+
+	}
+
+}
