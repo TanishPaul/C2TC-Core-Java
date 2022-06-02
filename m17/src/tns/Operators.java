f0641fcbb31b3fde88b99abf2a6cb51d4fd981b1
TanishPaul <tanishpaul26@gmail.com>
2022-06-02 10:54:48 +0530

    Second Commit

---
+++ m17/src/tns/Operators.java
@@ -0,0 +1,12 @@
+package tns;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=++x;
+		System.out.print(y);
+	}
+
+}
---
+++ m17/src/tns/operators2.java
@@ -0,0 +1,14 @@
+package tns;
+
+public class operators2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=++x;
+		System.out.print("Final value of x=" + x);
+		System.out.print("Value of y=" + y);
+		
+	}
+
+}
